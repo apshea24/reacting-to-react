d88a241dc9706db2a3eea9358734fa4b17191a54
shaggsworth <austin.shea89@gmail.com>
2019-04-24 13:45:48 -0700

    links_on_contact_page

--- app/src/components/pages/contact/index.js
+++ app/src/components/pages/contact/index.js
@@ -1,44 +1,41 @@
 import React from "react";
 
-import { Hero, HeroBody, Title, Container, Card, Content, Media, CardContent, CardHeader, CardImage, Image, MediaLeft, MediaContent, Subtitle, CardHeaderTitle } from "bloomer";
+import {
+    Hero, HeroBody, Title, Container, Card, CardContent, CardHeaderTitle, Field, FieldLabel, FieldBody,
+    Label, Icon, Input, Control, TextArea, Button
+} from "bloomer";
 import profileImg from "../../../styles/Images/Profile_Placeholder.jpg"
 
 function Contact(props) {
     return (
-      <div>
-        <Hero isColor="primary" isSize="medium" isBold="true" >
-          <HeroBody hasTextAlign="centered">
-            <Title>Contact Me!</Title>
-            <p style={{ maxWidth: 600, margin: "auto" }}>
-              Hello, my name is Austin Shea, and I am a Full Stack Web Developer with a focus on react based applications using the MERN Stack. I am currently an Assistant Instructor with the Univerisity of Arizona Coding Bootcamp. Welcome to my site.
+        <div>
+            <Hero isColor="primary" isSize="medium" isBold="true" >
+                <HeroBody hasTextAlign="centered">
+                    <Title>Contact Me!</Title>
+                    <p style={{ maxWidth: 600, margin: "auto" }}>
+                        Hello, my name is Austin Shea, and I am a Full Stack Web Developer with a focus on react based applications using the MERN Stack. I am currently an Assistant Instructor with the Univerisity of Arizona Coding Bootcamp. Welcome to my site.
             </p>
-          </HeroBody>
-        </Hero>
-        <Container style={{marginTop: "25px"}}>
-        {/* <Card className="has-background-grey has-text-primary">
-        <CardHeaderTitle>
-        <Title isSize={4}>Austin Shea</Title>
-        </CardHeaderTitle>
+                </HeroBody>
+            </Hero>
+            <Container style={{ marginTop: "25px" }}>
+                <Card className="has-background-grey has-text-primary">
 
-    <CardContent>
-        <Media>
-            <MediaLeft>
-                <Image isSize='30x30' src={profileImg} />
-            </MediaLeft>
-            <MediaContent>
-                
-            </MediaContent>
-        </Media>
-        <Content>
-            People Keep Asking If I’m Back, And I Haven’t Really Had An Answer, But Now, Yeah, I’m Thinking I’m Back.
-            <br/>
-            <small>11:09 PM - 30 October 2014</small>
-        </Content>
-    </CardContent>
-</Card> */}
-        </Container>
-      </div>
+
+                    <CardContent style={{textAlign: "center"}}>
+                        <h3>Connect with me on LinkedIn</h3>
+                        <a href="https://www.linkedin.com/in/austin-shea-790b2416b/"><Icon className="fab fa-linkedin-in" /></a>
+                        <h3>Follow Me on Twitter</h3>
+                        <a href="https://twitter.com/AlgusDark"><Icon className='fab fa-twitter' style={{ color: '#55acee' }} /></a>
+                        <h3>Check out my Github</h3>
+                        <a href="https://github.com/apshea24"><Icon className='fab fa-github' /></a>
+                        <h3>Leave me a Message</h3>
+                        <Icon className="fas fa-envelope-open-text" />
+
+                    </CardContent>
+                </Card>
+            </Container>
+        </div>
     );
-  }
-  
-  export default Contact;
+}
+
+export default Contact;
